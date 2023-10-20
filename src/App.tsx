e1f91b3bf43d170c4b05053927ba71a411a576cc
Ola Abuhasan <olaabuhasan.cs@gmail.com>
2023-10-20 13:57:06 +0300

    another test commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test commit 
+          Learn React
         </a>
       </header>
     </div>
